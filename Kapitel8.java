7798c91a77487addbec48a747573bc0d04b3d90f
Rasmus Mattas <mara16@nti.umea>
2017-10-16 11:17:11 +0200

    8.1 - 8.2

---
+++ Kapitel8.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+public class Kapitel8{
+	public static void main(String[] args){
+		/*String rs = JOptionPane.showInputDialog(null, "Vad e radien?");
+		double r = Double.parseDouble(rs);
+		double A, O;
+		A = Cirkel.area(r);
+		O = Cirkel.omk(r);
+		JOptionPane.showMessageDialog(null, "Omkrets: " + O + "\nArea: " + A);*/
+
+		/*String ys = JOptionPane.showInputDialog(null, "Vilket ar ar det?");
+		int y = Integer.parseInt(ys);
+		boolean yb = Kalender.skottar(y);
+		if(yb == true){
+			JOptionPane.showMessageDialog(null, "Det ar skottar!");
+		}else if(yb == false){
+			JOptionPane.showMessageDialog(null, "Inte skottar!");
+		}*/
+	}
+}
+/*
+class Cirkel{
+	public static double area(double a){
+		return a * a * Math.PI;
+	}
+	public static double omk(double a){
+		return a * 2 * Math.PI;
+	}
+}*/
+/*class Kalender{
+	public static boolean skottar(int Year){
+		if(Year%4 == 0 && Year%400 != 0){
+			return true;
+		}else {
+			return false;
+		}
+	}
+}*/
